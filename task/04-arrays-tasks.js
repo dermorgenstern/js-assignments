8419ce0ac7c15555bcc7161da79547e20dd684cf
dermorgenstern <diemorgenstern@gmail.com>
2021-04-09 10:56:07 -0400

    Update the links

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -535,6 +535,7 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2]) 
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
+
 function getElementByIndexes(arr, indexes) {
     throw new Error('Not implemented');
 }
